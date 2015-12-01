236931a7b4f0224b372cc6e3abd5a224edb8f5a7
JRyuzaki <zivanovicjovanprivatemail@gmail.com>
2015-12-01 15:30:23 +0100

    Made Abstract

--- Holzbau.java
+++ Holzbau.java
@@ -1,6 +1,6 @@
 import java.util.LinkedList;
 
-public class Holzbau extends Dachstuhl{
+public abstract class Holzbau extends Dachstuhl{
 	public Holzbau(int laenge, int amount) {
 		super(laenge, amount);
 	}
