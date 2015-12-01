1d4fe1d3b5386e3c62757a07b3fdee323601333c
apuffler <a.puffler@gmail.com>
2015-12-01 14:50:42 +0100

    Stuffs

--- Test.java
+++ Test.java
@@ -1,3 +1,10 @@
+/*
+Armin Puffler (1225268): Assertions in all Classes, Zimmerei.java
+
+
+*/
+
+
 
 public class Test {
 	
---
+++ Zimmerei.java
@@ -0,0 +1,10 @@
+public class Zimmerei
+{
+	
+	public Zimmerei ()
+	{
+
+		
+	}
+
+}
