8dc80a0cbe1addf17c9072db2d6009b8a1b203a9
apuffler <a.puffler@gmail.com>
2015-12-01 15:22:22 +0100

    Added Zimmerei and Asserts

--- Zimmerei.java
+++ Zimmerei.java
@@ -1,10 +1,48 @@
 public class Zimmerei
 {
 	
-	public Zimmerei ()
+	Lager lager = null;
+	
+	//ASSERT: Lager lager must not be NULL!
+	public Zimmerei (Lager lager)
 	{
+		this.lager = lager;
+	}
 
-		
+	//ASSERT: Lager l must not be NULL!
+	public setLager (Lager l)
+	{
+		this.lager = l;
+	}
+	//ASSERT: Dachstuhl dachst must not be NULL!
+	public Dachstuhl construct(Dachstuhl dachst)
+	{
+		dachst.bauholzMaterial.size()
+
+		while (dachst.bauholzMaterial.size() > 0)
+		{
+			
+			Bauholz b = this.lager.get(dachst);
+
+			//Dachstuhl couldn't be finished, return all material.
+			if(b == null ) // && !this.dachst.isFinished 
+			{
+				//Return all the Bauholu
+				for (Bauholz r : this.lager.bauholzMaterial)
+				{
+					this.lager.add(r);
+				}
+				this.lager.bauholzLager = new LinkedList<Bauholz>();
+			}
+
+			//this.dachst.add
+
+		}
+
+
+		//Finished 
+		return dachst;
+
 	}
 
 }
